7f790e15228175a29ab5451f077ace92ea2a0c4c
Martin <martin.emery@sfr.fr>
2021-11-22 18:06:50 +0100

    Creation class Main, Message, ConversationManager

---
+++ code/ConversationManager.java
@@ -0,0 +1,9 @@
+public class ConversationManager {
+    
+    Thread conversations[]; // Tableau de toutes les conversartions en cours
+
+    void start_conversation(User user1, User user2){
+        
+    }
+
+}
---
+++ code/Message.java
@@ -0,0 +1,13 @@
+import java.time.*;
+
+public class Message {
+    public User from; // User écrivant le message
+    public User to;   // User recevant le message
+    protected String msg; // Texte du message
+    protected LocalDateTime timestamp; // Horodage
+
+    // Constructeur message
+    Message(User from, User to, String msg){
+        this.from=from; this.to=to; this.msg=msg; this.timestamp = LocalDateTime.now();
+    }
+}
