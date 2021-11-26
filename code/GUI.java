6e3544d464e17a7cf958330b7e12e5feed275b90
Martin <martin.emery@sfr.fr>
2021-11-26 08:09:38 +0100

    ajout de GUI

---
+++ code/GUI.java
@@ -0,0 +1,99 @@
+/*
+ * SwingApplication.java is a 1.4 example that requires
+ * no other files.
+ */
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class GUI implements ActionListener {
+    private static String labelPrefix = "Welcome on M&M's chat system ";
+    
+    private int numClicks = 0;
+    final JLabel label = new JLabel(labelPrefix + "0    ");
+
+    
+    public Component createComponents() {
+        JButton button = new JButton("Open chat");
+        button.setMnemonic(KeyEvent.VK_I);
+        button.addActionListener(this);
+        label.setLabelFor(button);
+
+        // Create a second button. The same event handler will be used. 
+        /*JButton button2 = new JButton("I'm 2nd Swing button!"); 
+        button2.setMnemonic(KeyEvent.VK_L); 
+        button2.addActionListener(this); 
+        label.setLabelFor(button2); 
+        */
+
+        
+        /*
+         * An easy way to put space between a top-level container
+         * and its contents is to put the contents in a JPanel
+         * that has an "empty" border.
+         */
+        JPanel pane = new JPanel(new GridLayout(0, 1));
+        pane.add(button);
+        //Add 2nd button to the JPanel 
+        //pane.add(button2); 
+        pane.add(label);
+        /*pane.setBorder(BorderFactory.createEmptyBorder(
+                30, //top
+                30, //left
+                10, //bottom
+                30) //right
+                );
+        */
+        return pane;
+    }
+
+     
+    // Modify the event handler code depending on which button is pressed. 
+    // If the 1st button is pressed, increase the numClicks value by 1, else 
+    // increase the value by 1000. 
+    public void actionPerformed(ActionEvent e) { 
+ 
+        // Using getActionCommand() method is a bit of a hack, but for the 
+        // sake of this exercise, it serves its purpose. 
+        if (e.getActionCommand().equals("I'm a Swing button!")){ 
+            numClicks++; 
+        } 
+        else{ 
+            numClicks += 1000; 
+        } 
+        label.setText(labelPrefix + numClicks); 
+    } 
+
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        
+        //Make sure we have nice window decorations.
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        
+        //Create and set up the window.
+        JFrame frame = new JFrame("SwingApplication");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        GUI app = new GUI();
+        Component contents = app.createComponents();
+        frame.getContentPane().add(contents, BorderLayout.CENTER);
+        
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
