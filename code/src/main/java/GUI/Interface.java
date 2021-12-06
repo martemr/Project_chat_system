5a93562ca397cd1532e9e012e9e131a3ee423c7f
MariePontalier <mpontali@etud.insa-toulouse.fr>
2021-12-06 10:28:39 +0100

    Interface : essai responsive

--- code/src/main/java/GUI/Interface.java
+++ code/src/main/java/GUI/Interface.java
@@ -1,6 +1,5 @@
 package GUI;
 
-import java.awt.BorderLayout;
 import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
@@ -31,9 +30,6 @@ public class Interface implements ActionListener {
     Timestamp ts;
     Date date;
 
-    final static boolean shouldFill = true;
-    final static boolean shouldWeightX = true;
-
     public void createPannels(){
         // Create the main panel
         //  There is only one panel at this step
@@ -54,10 +50,7 @@ public class Interface implements ActionListener {
         interfaceFrame.setSize(800, 600);
         interfaceFrame.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
-        if (shouldFill) {
-        //natural height, maximum width
-        c.fill = GridBagConstraints.HORIZONTAL;
-        }
+
 
         //Create user
         user = new User("titi");
@@ -65,10 +58,7 @@ public class Interface implements ActionListener {
         // Pseudo field
         pseudoLabel = new JLabel("Pseudo :");
         //pseudoLabel.setBounds(0, 0, 100, 30);  
-        if (shouldWeightX) {
-            c.weightx = 0.5;
-            }
-            c.fill = GridBagConstraints.HORIZONTAL;
+            c.fill = GridBagConstraints.VERTICAL;
             c.gridx = 0;
             c.gridy = 0;
         interfaceFrame.add(pseudoLabel, c);
@@ -76,8 +66,8 @@ public class Interface implements ActionListener {
         pseudoCapture=new JTextField(user.pseudo);
         //pseudoCapture.setBounds(100, 0, 500,30);  
         pseudoCapture.addActionListener(this); // capture le retour chariot
-        c.fill = GridBagConstraints.HORIZONTAL;
-	    c.weightx = 0.5;
+        c.fill = GridBagConstraints.VERTICAL;
+	    //c.weightx = 0.5;
 	    c.gridx = 1;
 	    c.gridy = 0;
         interfaceFrame.add(pseudoCapture, c); // lie la capture à la fenetre
@@ -85,8 +75,8 @@ public class Interface implements ActionListener {
         changePseudoButton=new JButton("Change Pseudo");
         //changePseudoButton.setBounds(600, 0, 100,30);
         changePseudoButton.addActionListener(this);
-        c.fill = GridBagConstraints.HORIZONTAL;
-	    c.weightx = 0.5;
+        c.fill = GridBagConstraints.VERTICAL;
+	    //c.weightx = 0.5;
 	    c.gridx = 2;
 	    c.gridy = 0;
         interfaceFrame.add(changePseudoButton,c);
@@ -94,8 +84,8 @@ public class Interface implements ActionListener {
         // Message fields
         messageLabel = new JLabel("Message :");
        // messageLabel.setBounds(0, 30, 100,30);
-       c.fill = GridBagConstraints.HORIZONTAL;
-	    c.weightx = 0.5;
+       c.fill = GridBagConstraints.VERTICAL;
+	    //c.weightx = 0.5;
 	    c.gridx = 0;
 	    c.gridy = 2;
         interfaceFrame.add(messageLabel, c);
@@ -103,8 +93,8 @@ public class Interface implements ActionListener {
         msgCapture=new JTextField();
        // msgCapture.setBounds(100, 30, 500,30);  
         msgCapture.addActionListener(this); // capture le retour chariot
-        c.fill = GridBagConstraints.HORIZONTAL;
-	    c.weightx = 0.5;
+        c.fill = GridBagConstraints.VERTICAL;
+	    //c.weightx = 0.5;
 	    c.gridx = 1;
 	    c.gridy = 2;
         interfaceFrame.add(msgCapture, c);
@@ -112,8 +102,8 @@ public class Interface implements ActionListener {
         sendMessageButton=new JButton("Send");
        // sendMessageButton.setBounds(600, 30, 100, 30);
         sendMessageButton.addActionListener(this); // Capture le clic sur le bouton L'instruction this indique que la classe elle même recevra et gérera l'événement utilisateur.
-        c.fill = GridBagConstraints.HORIZONTAL;
-	    c.weightx = 0.5;
+        c.fill = GridBagConstraints.VERTICAL;
+	   // c.weightx = 0.5;
 	    c.gridx = 2;
 	    c.gridy = 2;
         interfaceFrame.add(sendMessageButton, c);
@@ -122,9 +112,9 @@ public class Interface implements ActionListener {
         displayMsg =new JTextArea("CONVERSATION \n \n");
         displayMsg.setEditable(false); // Bloque l'édition de la zone de texte   
         scroll = new JScrollPane(displayMsg); 
-        c.fill = GridBagConstraints.HORIZONTAL;
-	    c.ipady = 40;      //make this component tall
-	    c.weightx = 0.0;
+        c.fill = GridBagConstraints.VERTICAL;
+	    c.ipady = 50;      //make this component tall
+	   // c.weightx = 0.0;
 	    c.gridwidth = 3;
 	    c.gridx = 0;
 	    c.gridy = 1; 
