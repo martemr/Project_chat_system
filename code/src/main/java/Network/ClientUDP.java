299a76dee3b89956d32f44fafda63a9d633a9b48
Martin <martin.emery@sfr.fr>
2022-01-06 10:53:12 +0100

    UDP : Brodcasast fait

--- code/src/main/java/Network/ClientUDP.java
+++ code/src/main/java/Network/ClientUDP.java
@@ -1,15 +1,57 @@
 package Network;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.util.Scanner;
+import java.net.UnknownHostException;
+
+import GUI.User;
+import Main.Main;
 
 public class ClientUDP {
 
+    DatagramSocket socket;
+    InetAddress IPAddress;
+    byte[] incomingData;
+    User user;
 
     public ClientUDP() throws IOException {
+        socket = new DatagramSocket();
+        IPAddress = InetAddress.getByName("localhost");
+        incomingData = new byte[1024];
+        user = Main.getMainUser();
+    }
+    
+    /** Function who will send the user as broadcast */
+    public void sendBroadcast(){
+        try{
+            socket.setBroadcast(true); // Send a broadcast
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            ObjectOutputStream os = new ObjectOutputStream(outputStream);
+            os.writeObject(user);
+            incomingData = outputStream.toByteArray();
+            DatagramPacket sendPacket = new DatagramPacket(incomingData, incomingData.length, IPAddress, 1234);
+            socket.send(sendPacket);
+            System.out.println("[UDP Client] Broadcast message sent");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+       
+            /*
+            DatagramPacket incomingPacket = new DatagramPacket(incomingData, incomingData.length);
+            Socket.receive(incomingPacket);
+            String response = new String(incomingPacket.getData());
+            System.out.println("Response from server:" + response);
+            Thread.sleep(2000);
+            */
+           
+
+/*
         // Create the socket object for carrying the data.
         DatagramSocket datagramReceiveSocket = new DatagramSocket();
     
@@ -22,7 +64,4 @@ public class ClientUDP {
 
         // invoke the send call to actually send the data.
         datagramReceiveSocket.send(broadcastPacket);
-
-    }
-
-}
+*/
--- code/src/main/java/Network/ServerUDP.java
+++ code/src/main/java/Network/ServerUDP.java
@@ -1,49 +1,70 @@
 package Network;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 
+import GUI.User;
 
-public class ServerUDP {
 
-    // A utility method to convert the byte array
-    // data into a string representation.
-    public static StringBuilder data(byte[] a)
-    {
-        if (a == null)
-            return null;
-        StringBuilder ret = new StringBuilder();
-        int i = 0;
-        while (a[i] != 0)
-        {
-            ret.append((char) a[i]);
-            i++;
-        }
-        return ret;
+public class ServerUDP extends Thread {
+
+    DatagramSocket socket;
+    byte[] incomingData;
+    DatagramPacket incomingPacket;
+
+    public ServerUDP(int port) throws IOException {
+        socket = new DatagramSocket(1234);
+        incomingData = new byte[65535];
+        incomingPacket = null;
     }
 
-    public ServerUDP() throws IOException{
-        // Step 1 : Create a socket to listen at port 1234
-        DatagramSocket ds = new DatagramSocket(1234);
-        byte[] receive = new byte[65535];
-        
-        DatagramPacket DpReceive = null;
-        while (true)
-        {
-    
-            // Step 2 : create a DatgramPacket to receive the data.
-            DpReceive = new DatagramPacket(receive, receive.length);
-    
-            // Step 3 : revieve the data in byte buffer.
-            ds.receive(DpReceive);
-    
-            System.out.println("Client:-" + data(receive));
-    
-            // Clear the buffer after every message.
-            receive = new byte[65535];
+    public void run() {
+        try{
+            System.out.println("[UDP Server] Starting server UDP ");
+            while (true)
+            {
+                incomingPacket = new DatagramPacket(incomingData, incomingData.length);
+                socket.receive(incomingPacket);
+                byte[] data = incomingPacket.getData();
+                ByteArrayInputStream in = new ByteArrayInputStream(data);
+                ObjectInputStream is = new ObjectInputStream(in);
+                try {
+                    User user = (User) is.readObject();
+                    System.out.println("[UDP Server] Broadcast received from " + user.pseudo);
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
+            /*
+            InetAddress IPAddress = incomingPacket.getAddress();
+            int port = incomingPacket.getPort();
+            String reply = "Thank you for the message";
+            byte[] replyBytea = reply.getBytes();
+            DatagramPacket replyPacket =
+            new DatagramPacket(replyBytea, replyBytea.length, IPAddress, port);
+            socket.send(replyPacket);
+            Thread.sleep(2000);
+            System.exit(0);
+            }
+            // Step 2 : create a DatgramPacket to receive the data.
+            DpReceive = new DatagramPacket(receive, receive.length);
+            
+            // Step 3 : revieve the data in byte buffer.
+            ds.receive(DpReceive);
+            
+            System.out.println("Client:-" + data(receive));
+            
+            // Clear the buffer after every message.
+            receive = new byte[65535];
+        }
+        */  
