69ca918cf812c8bcc41e00000249ebafef440538
Martin <martin.emery@sfr.fr>
2022-01-07 15:47:53 +0100

    Main : Ajout d'un test

--- code/src/main/java/Main/Main.java
+++ code/src/main/java/Main/Main.java
@@ -1,6 +1,7 @@
 package Main;
 
 import java.io.IOException;
+import java.util.Vector;
 
 import GUI.*;
 import Database.*;
@@ -14,9 +15,14 @@ public class Main {
     static DatabaseManager database;
     static ServerTCP tcpServer;
     static ClientTCP tcpClient; // TODO : Link with interface
+    static ServerUDP udp_server;
+    static final int UDPPort=1234;
+    static final int TCPPort=4321;
+
+    static public Vector<User> connectedUsers;
 
     static {
-        //user = new User("Pseudo"); // the main user is declared once, here.
+        user = new User("Pseudo"); // the main user is declared once, here.
         //database = new DatabaseManager();
     }
 
@@ -34,33 +40,51 @@ public class Main {
         return tcpClient;
     }
 
+    static public ServerUDP getServerUDP(){
+        return udp_server;
+    }
+
+    public static void startUDPServer(){
+        try{
+            ServerUDP udp_server = new ServerUDP(UDPPort);
+            udp_server.start();
+            ClientUDP udp_client=new ClientUDP();
+            udp_client.sendBroadcast();
+        } catch (IOException e) {
+            System.out.println("[Main] Error while starting UDP");
+        }
+    }
+
+    public static void startTCPServer(){
+        try {
+            tcpServer = new ServerTCP(TCPPort); // Start a thread on given server, ready to wait for messages
+            tcpServer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     // Main fonction (appelée en premier lors de l'exécution)
     public static void main(String[] args) {     
 
         //database.testdb();
         
-        //System.out.println("[Main] Starting interface");
-        //mainWindow = new Interface();   
+        // Démarre le serveur UDP sur le port 1234
+        //System.out.println("[Main] Starting server UDP ");
+        //startUDPServer();
+
+        User Martin = new User("Martin", 60, User.Status.ABSENT);
+        User Paul = new User("Paul", 79, User.Status.OCCUPIED);
+        connectedUsers = new Vector<User>();
+        User Marie = new User("Marie", 33, User.Status.CONNECTED);
+        connectedUsers.add(Martin);
+        connectedUsers.add(Paul);
+        connectedUsers.add(Marie);
+
+        System.out.println("[Main] Starting interface");
+        mainWindow = new Interface();   
         
-        // TCP Server
 
-        //System.out.println("[Main] Running the server");
-        //int port = 1234;
-        //try {
-        //    tcpServer = new ServerTCP(port); // Start a thread on given server, ready to wait for messages
-        //    tcpServer.start();
-        //} catch (IOException e) {
-        //    e.printStackTrace();
-        //}
-
-
-        // UDP  
-        try{
-            ClientUDP udp = new ClientUDP();
-        } catch (IOException e) {
-            System.out.println("Error in udp");
-        }
-        
     }
 
     public void messageReceived(Message msg){
@@ -68,6 +92,6 @@ public class Main {
     }
 
     public static void closeSystem(){
-        database.closeConnection();
+        //database.closeConnection();
     }
 }
