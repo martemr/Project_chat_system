58f3d64c60711b15ac09ed278235f71ac1e8f3c6
MariePontalier <mpontali@etud.insa-toulouse.fr>
2022-01-07 17:26:12 +0100

    Tools : finalement ce sera xml

---
+++ code/src/main/java/tools.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.xml.*;
+
+public class tools {
+    public String lire_json(String quoilire){
+        JSONParser jsonP = new JSONParser();
+
+        String lecture = "";
+        return lecture;
+    }
+}
