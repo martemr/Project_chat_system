16eddc1a76de70df6543e37c258f1949ba5c774e
MariePontalier <mpontali@etud.insa-toulouse.fr>
2022-01-10 10:49:36 +0100

    Tools : lecture du fichier config.xml

---
+++ code/src/main/java/Main/Tools.java
@@ -0,0 +1,62 @@
+package Main;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+import javax.xml.parsers.*;
+
+
+public class Tools {
+
+    public static void lire_config_xml(){ 
+        try {
+            //Obtenir la configuration du sax parser
+            SAXParserFactory spfactory = SAXParserFactory.newInstance();
+            //Obtenir une instance de l'objet parser
+            SAXParser saxParser = spfactory.newSAXParser();
+     
+            /*les trois méthodes sont déclarées dans le
+             corp du DefaltHandler*/
+            DefaultHandler handler = new DefaultHandler() {
+                boolean ip_local = false;
+                boolean ip_broadcast = false;
+         
+                /*cette méthode est invoquée à chaque fois que parser rencontre
+                  une balise ouvrante '<' */
+                public void startElement(String uri, String localName,String qName,Attributes attributes) throws SAXException{
+                   if (qName.equalsIgnoreCase("ip_local")) {
+                     ip_local = true;
+                   } else if (qName.equalsIgnoreCase("ip_broadcast")) {
+                     ip_broadcast = true;
+                   }
+                }
+         
+                /*cette méthode est invoquée à chaque fois que parser rencontre
+                  une balise fermante '>' */
+                public void endElement(String uri, String localName,String qName) throws SAXException {    
+                   if (qName.equalsIgnoreCase("ip_local")) {
+                     ip_local = false;
+                   } else if (qName.equalsIgnoreCase("ip_broadcast")) {
+                     ip_broadcast = false;
+                   }
+                }
+        
+                //TODO: traiter les données
+                public void characters(char ch[], int start,int length) throws SAXException {
+                   if (ip_local) {
+                     System.out.println("ip_local : " + 
+                            new String(ch, start, length));
+                     ip_local = false;
+                   } else if (ip_broadcast) {
+                     System.out.println("ip_broadcast : " +
+                             new String(ch, start, length));
+                     ip_broadcast = false;
+                   }
+                }
+            };
+               saxParser.parse("config.xml", handler);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
